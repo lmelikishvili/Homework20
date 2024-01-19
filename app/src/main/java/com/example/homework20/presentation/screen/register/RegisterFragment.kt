ef00db27a975431ee24f9bccd4926919a7abda9a
lmelikishvili <levanimelikishvili@gmail.com>
2024-01-20 02:19:53 +0400

    before room database

--- app/src/main/java/com/example/homework20/presentation/screen/register/RegisterFragment.kt
+++ app/src/main/java/com/example/homework20/presentation/screen/register/RegisterFragment.kt
@@ -1,12 +1,25 @@
 package com.example.homework20.presentation.screen.register
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.example.homework20.R
 import com.example.homework20.databinding.FragmentRegisterBinding
 import com.example.homework20.presentation.base.BaseFragment
+import com.example.homework20.presentation.event.NavigationEvent
+import com.example.homework20.presentation.screen.welcome.WelcomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
+    private val viewModel: RegisterViewModel by viewModels()
     override fun setup() {
-
+        binding.btnAdd.setOnClickListener{
+            viewModel.onEvent(NavigationEvent.NavigateToLogin)
+        }
     }
 
     override fun setupListeners() {
@@ -14,7 +27,20 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterB
     }
 
     override fun bindData() {
-
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiEvent.collect {
+                    handleNavigationEvents(event = it)
+                }
+            }
+        }
     }
 
+    private fun handleNavigationEvents(event: RegisterViewModel.LogInUiEvent) {
+        when (event) {
+            is RegisterViewModel.LogInUiEvent.NavigateToLogin -> findNavController().navigate(
+                R.id.action_registerFragment_to_welcomeFragment
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/homework20/presentation/screen/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/homework20/presentation/screen/register/RegisterViewModel.kt
@@ -1,7 +1,30 @@
 package com.example.homework20.presentation.screen.register
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.homework20.presentation.event.NavigationEvent
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 
-class RegisterViewModel: ViewModel() {
+class RegisterViewModel : ViewModel() {
+    private val _uiEvent = MutableSharedFlow<LogInUiEvent>()
+    val uiEvent: SharedFlow<LogInUiEvent> get() = _uiEvent
 
+    fun onEvent(event: NavigationEvent) {
+        when (event) {
+            is NavigationEvent.NavigateToLogin -> toLogin()
+            else -> println()
+        }
+    }
+
+    private fun toLogin(){
+        viewModelScope.launch {
+            _uiEvent.emit(LogInUiEvent.NavigateToLogin)
+        }
+    }
+
+    sealed interface LogInUiEvent {
+        data object NavigateToLogin : LogInUiEvent
+    }
 }
